6424d0ff7de057042309beba1570511f1e1b1270
44x1carbon <b4079@oic.jp>
2019-05-01 22:45:04 +0900

    android websocket対応

--- android/app/src/main/java/com/example/pulpunte/android/MainActivity.kt
+++ android/app/src/main/java/com/example/pulpunte/android/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.pulpunte.android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.socket.client.IO
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val socket = IO.socket("https://c8a5d154.ngrok.io").connect()
     }
 }
