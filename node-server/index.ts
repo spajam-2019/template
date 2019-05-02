39bd77d49e44da1bc178268a87e0bf10286dd654
44x1carbon <b4079@oic.jp>
2019-05-02 20:15:20 +0900

    node test api作成

--- node-server/index.ts
+++ node-server/index.ts
@@ -8,6 +8,16 @@ app.get('/', (req: Express.Request, res: Express.Response) => {
   return res.send('Hello world.');
 });
 
+app.get('/json', (req: Express.Request, res: Express.Response) => {
+  class ResponseData {
+    num: Number;
+    constructor(num: Number) {
+      this.num = num;
+    }
+  }
+  return res.send(new ResponseData(2));
+});
+
 io.on("connection", function(socket: any) {
   console.log("a user connected");
 });
