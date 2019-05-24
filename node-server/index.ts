6e7896e4c53d5cc1fd63a2b9c1faca89932faaea
elipmoc <relipmoc101@gmail.com>
2019-05-25 08:18:23 +0900

    httpの型整備

--- node-server/index.ts
+++ node-server/index.ts
@@ -1,10 +1,11 @@
 import * as Express from 'express';
 import { routerMain } from './routers/routerMain';
 import * as SocketIo from 'socket.io';
+import { Server } from "http";
 
 const PORT = 3000;
 const app = Express();
-const http = require('http').Server(app);
+const http = new Server(app);
 const io = SocketIo(http);
 
 app.use(routerMain);
