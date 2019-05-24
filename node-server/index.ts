2c4de24c021873f19aa024c34646c15e28b05ffc
elipmoc <relipmoc101@gmail.com>
2019-05-25 07:43:45 +0900

    サーバーのポート番号を変数で共通化

--- node-server/index.ts
+++ node-server/index.ts
@@ -1,5 +1,6 @@
 import * as Express from 'express';
 
+const PORT = 3000;
 const app = Express();
 const http = require('http').Server(app);
 const io = require("socket.io")(http);
@@ -18,12 +19,12 @@ app.get('/json', (req: Express.Request, res: Express.Response) => {
   return res.send(new ResponseData(2));
 });
 
-io.on("connection", function(socket: any) {
+io.on("connection", function (socket: any) {
   console.log("a user connected");
 });
 
-const server = http.listen(3000, function() {
-  console.log("listening on *:3000");
+const server = http.listen(PORT, function () {
+  console.log(`listening on *:${PORT}`);
 });
 
 export default server;
